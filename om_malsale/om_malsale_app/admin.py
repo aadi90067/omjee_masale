b658e7b9f05143fcc0959ffc612eaa408d7faaf3
Aditya raj <adityarajraj5555@gmail.com>
2026-03-20 07:51:42 +0530

    ad

--- om_malsale/om_malsale_app/admin.py
+++ om_malsale/om_malsale_app/admin.py
@@ -5,68 +5,12 @@ from .models import Product, Order
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "image_preview",
-        "name",
-        "mrp",
-        "pack1_price",
-        "pack3_price",
-        "pack6_price",
-        "rating",
-        "review_count",
-        "discount_percent",
-        "discount_text",
-    )
-
-    search_fields = ("name",)
-    list_editable = ("rating", "review_count", "discount_percent", "discount_text")
-    readonly_fields = ("image_preview_large",)
-
-    fieldsets = (
-        ("Product Details", {
-            "fields": ("name", "image", "image_preview_large")
-        }),
-        ("Pricing", {
-            "fields": ("mrp", "pack1_price", "pack3_price", "pack6_price")
-        }),
-        ("Rating & Reviews", {
-            "fields": ("rating", "review_count")
-        }),
-        ("Offer / Discount", {
-            "fields": ("discount_percent", "discount_text")
-        }),
-    )
-
-    def image_preview(self, obj):
-        try:
-            if obj.image:
-                return format_html(
-                    '<img src="{}" width="55" height="55" style="border-radius:8px; object-fit:cover;" />',
-                    obj.image.url
-                )
-        except:
-            pass
-        return "-"
-    image_preview.short_description = "Image"
-
-    def image_preview_large(self, obj):
-        try:
-            if obj.image:
-                return format_html(
-                    '<img src="{}" width="180" style="border-radius:12px; object-fit:cover;" />',
-                    obj.image.url
-                )
-        except:
-            pass
-        return "-"
-    image_preview_large.short_description = "Preview"
+    list_display = ("id", "name", "mrp", "pack1_price", "pack3_price", "pack6_price")
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "phone", "show_items", "total", "created")
-    search_fields = ("name", "phone")
     readonly_fields = ("name", "phone", "address", "show_full_items", "total", "created")
 
     fieldsets = (
@@ -80,10 +24,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     def show_items(self, obj):
         if obj.items:
-            first_line = obj.items.split("\n")[0]
-            if len(obj.items.split("\n")) > 1:
-                return f"{first_line} ..."
-            return first_line
+            return obj.items
         return "-"
     show_items.short_description = "Items"
 
